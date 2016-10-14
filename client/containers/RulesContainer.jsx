0ffef85139968e6bd034ed973dfc968d2ee5d012
Sandrino Di Mattia <contact@sandrino.be>
2016-10-14 11:48:38 +0200

    Front err handling (#5) (#6)
    * qa build
    
    * git status
    
    * qa build
    
    * production build
    
    * tools + rules management
    
    * route fix
    
    * fixed rules post
    
    * rules for TFS
    
    * production build
    
    * qa build
    
    * bundle rebuild
    
    * test
    
    * prod build
    
    * auth0 version updated
    
    * tests
    
    * pages deployment
    
    * pages deployment
    
    * eslint
    
    * prod build
    
    * code linting
    
    * code review.Fixes
    
    * source-control-tools; linting
    
    * gitlabWebhook middleware fixed
    
    * unit tests
    
    * unit tests
    
    * unit tests
    
    * unit tests
    
    * alerts
    
    * merge changes
    
    * firstrun notification; linting
    
    * unit tests
    
    * published tools
    
    * middleware added

---
+++ client/containers/RulesContainer.jsx
@@ -0,0 +1,60 @@
+import React, { PropTypes, Component } from 'react';
+import connectContainer from 'redux-static';
+
+import { ruleActions } from '../actions';
+
+import { Error, LoadingPanel } from '../components/Dashboard';
+import RulesTable from '../components/RulesTable';
+
+export default connectContainer(class extends Component {
+  static stateToProps = (state) => ({
+    rules: state.rules.get('records'),
+    loading: state.rules.get('loading'),
+    error: state.rules.get('error'),
+    showNotification: state.rules.get('showNotification'),
+    notificationType: state.rules.get('notificationType')
+  });
+
+  static actionsToProps = {
+    ...ruleActions
+  }
+
+  static propTypes = {
+    rules: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired,
+    error: PropTypes.object.isRequired,
+    fetchAllRules: PropTypes.func.isRequired,
+    updateRules: PropTypes.func.isRequired
+  }
+
+  componentWillMount() {
+    this.props.fetchAllRules();
+  }
+
+  render() {
+    const rules = this.props.rules;
+    const loading = this.props.loading;
+    const error = this.props.error;
+    return (
+      <div>
+        <LoadingPanel show={loading} animationStyle={{ paddingTop: '5px', paddingBottom: '5px' }}>
+          <div className="row">
+            <div className="col-xs-12">
+              <Error message={error} />
+              <RulesTable
+                rules={rules}
+                loading={loading}
+                error={error}
+                saveManualRules={this.props.updateRules}
+                openNotification={this.props.openNotification}
+                closeNotification={this.props.closeNotification}
+                showNotification={this.props.showNotification}
+                notificationType={this.props.notificationType}
+              />
+            </div>
+          </div>
+        </LoadingPanel>
+      </div>
+    );
+  }
+});
--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -4,13 +4,17 @@ import promiseMiddleware from 'redux-promise-middleware';
 import { compose, createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../reducers';
+import normalizeErrorMiddleware from '../middlewares/normalizeErrorMiddleware';
 import DevTools from '../containers/DevTools';
+
+const nextRootReducer = ((process.env.NODE_ENV !== 'production' && module.hot)) ? require('../reducers') : null;
 
 export default function configureStore() {
   const pipeline = [
     applyMiddleware(
       promiseMiddleware(),
       thunkMiddleware,
+      normalizeErrorMiddleware(),
       createLogger({
         predicate: () => process.env.NODE_ENV !== 'production'
       })
@@ -25,9 +29,8 @@ export default function configureStore() {
   const store = finalCreateStore(rootReducer, { });
 
   // Enable Webpack hot module replacement for reducers.
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
+  if (nextRootReducer) {
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers');
       store.replaceReducer(nextRootReducer);
     });
   }
