195a844035e5e2b65bd366028812fca75ef64d53
Oleksandr Zarubin <oleksandr.zarubin@ua.akvelon.com>
2017-04-04 13:52:11 +0300

    tools updated

--- webtask.js
+++ webtask.js
@@ -1,9 +1,16 @@
-const tools = require('auth0-extension-tools');
+const tools = require('auth0-extension-express-tools');
 
 const expressApp = require('./server');
+const config = require('./server/lib/config');
 const logger = require('./server/lib/logger');
 
-module.exports = tools.createExpressServer((req, config, storage) => {
-  logger.info('Starting GitLab Deploy extension - Version:', config('CLIENT_VERSION'));
-  return expressApp(config, storage);
+const createServer = tools.createServer((cfg, storage) => {
+  logger.info('Starting GitLab Deploy extension - Version:', process.env.CLIENT_VERSION);
+  return expressApp(cfg, storage);
 });
+
+
+module.exports = (context, req, res) => {
+  config.setValue('PUBLIC_WT_URL', tools.urlHelpers.getWebtaskUrl(req));
+  createServer(context, req, res);
+};
