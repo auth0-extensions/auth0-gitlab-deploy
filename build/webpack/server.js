080bb7c45acb2c722893d448c19c92cd9e248015
Alexander Zarubin <nonexist.void@gmail.com>
2018-12-11 15:30:23 +0200

    client:build and dev-server are moved to extenions-cli

--- build/webpack/server.js
+++ build/webpack/server.js
@@ -1,48 +1,7 @@
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
+const devServer = require('auth0-extensions-cli/lib/dev-server');
 
-const config = require('./config.js');
-const logger = require('../../server/lib/logger');
+const rootPath = process.cwd();
+const entry = './client/app.jsx';
+const destination = './dist';
 
-logger.info('Running development webpack server...');
-
-const options = {
-  publicPath: 'http://localhost:3000/app/',
-  disableHostCheck: false,
-  hot: true,
-  inline: true,
-  historyApiFallback: true,
-  proxy: [
-    {
-      context: () => true,
-      target: {
-        port: 3001
-      }
-    }
-  ],
-
-  quiet: false,
-  noInfo: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
-  },
-
-  stats: { colors: true },
-  headers: {
-    'Access-Control-Allow-Origin': '*'
-  }
-};
-
-new WebpackDevServer(webpack(config), options)
-  .listen(3000, 'localhost',
-    (err) => {
-      if (err) {
-        logger.error(err);
-      } else {
-        logger.info('Development server listening on: http://localhost:3000');
-
-        // Start the actual webserver.
-        require('../../index');
-      }
-    });
+devServer(rootPath, entry, destination);
--- client/showDevTools.jsx
+++ client/showDevTools.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import DevTools from './containers/DevTools';
 
-module.exports = (store) => {
+export default (store) => {
   const popup = window.open(null, 'Redux DevTools', 'menubar=no,location=no,resizable=yes,scrollbars=no,status=no');
   popup.location.reload();
 
